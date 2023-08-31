c33cbdf83be4bc5dc903475b8379fdf7727f5df7
Shubham Kumar (Product Architecture) <unresolved.shubham@gmail.com>
2023-08-31 12:26:41 +0530

    Using the nif library

--- src/demo_app.erl
+++ src/demo_app.erl
@@ -7,27 +7,12 @@
 
 -behaviour(application).
 
--export([start/2, stop/1]).
+-export([start/2]).
 
 start(_StartType, _StartArgs) ->
-    Pid = aiml_model_wrapper:init("storable_to_pps_queue_model", "1.0.0", "python3"),
-    predict(Pid),
+    pmml_parser:load_model("storable_to_pps_queue_model", "1.0.0"),
+    X = #{"idc_time"=>"80","x_start"=>"50","x_goal"=>"50","y_start"=>"150","y_goal"=>"150"},
+    Y = pmml_parser:evaluate(X),
+    io:format("Input: ~p~n", [X]),
+    io:format("Output: ~s~n", [Y]),
     demo_sup:start_link().
-
-predict(Model)->
-    Features = sample:get_features_somehow(),
-    io:format("idc_time: ~p~n", [maps:get(idc_time, Features)]),
-    io:format("x_goal: ~p~n", [maps:get(x_goal, Features)]),
-    io:format("x_start: ~p~n", [maps:get(x_start, Features)]),
-    io:format("y_goal: ~p~n", [maps:get(y_goal, Features)]),
-    io:format("y_start: ~p~n", [maps:get(y_start, Features)]),
-
-    io:format("Starting Prediction~n"),
-    Result = aiml_model_wrapper:evaluate(Model, Features),
-    io:format("Result: ~p~n", [Result]),
-    aiml_model_wrapper:stop(Model).
-
-stop(_State) ->
-    ok.
-
-%% internal functions
